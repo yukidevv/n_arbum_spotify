93db44b8dacfd1546e983dc7f21d5f29947eeb71
yukidevv <nomad048@gmail.com>
2023-10-29 21:06:01 +0900

    update

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ def main():
   spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
   #date =''
   
-  track_list = Get_Track_id() #TrackID
-  
+  #TrackID(1曲分のみ)→AlbumID→TrackID→プレイリスト
+  #TrackIDからAlbumIDを特定。特定したAlbumIDからTrackID(複数)を取得してその数分プレイリストに突っ込む
+  track_list = Get_Track_id() #インプットファイルからTrackIDのみをListで取得
+  Album_List = Get_Album_id(track_list,spotify) #TrackIDからAlbumIDを取得
+
   #print(track_list)
 
 def Get_Track_id():
@@ -21,6 +24,12 @@ def Get_Track_id():
     for line in f:
       get_list.append(line.replace("spotify:track:",'').strip())
   return get_list
+
+def Get_Album_id(track_list,spotify):
+  for trackid in track_list:
+    track = spotify.track(trackid)
+    print(track)
+    #print("\r\n")
 
 def Read_Auth_Info():
   with open('./secret') as f:
